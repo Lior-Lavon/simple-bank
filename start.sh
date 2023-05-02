81b99f7ba80c9a0d24582eff3751c9c46d61beae
Lior Lavon <liorlavon@Liors-MacBook-Pro.local>
2023-05-02 14:05:36 +0200

    load enviroment variables before running db migration

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# load enviroment variables to shell
+echo "loading enviroment variables"
+source /app/app.env
+
 echo "run db migration"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
