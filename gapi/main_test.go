2c9a3dcb4dff4f1ef8edee81935b9862789791b5
Lior Lavon <liorlavon@Liors-MacBook-Pro.local>
2023-06-06 17:42:28 +0200

    test grpc auth

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/liorlavon/simplebank/db/sqlc"
+	"github.com/liorlavon/simplebank/token"
 	"github.com/liorlavon/simplebank/util"
 	"github.com/liorlavon/simplebank/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -21,4 +25,19 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+// create a new context and add auth token to metadata
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	// add a access token to the context metadata
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
